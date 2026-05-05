31b9de1e8f7213ee527c5b334d631697623ba679
manojkumar-tech267 <manojkumarparuchuri8@gmail.com>
2026-05-05 09:59:38 +0530

    security group module completed

---
+++ locals.tf
@@ -0,0 +1,14 @@
+locals {
+    common_tags = {
+        Project     = var.project
+        Environment = var.environment
+        Terraform = true
+    },
+    sg_final_tags = merge(
+    var.sg_tags,
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}-${var.sg_name}"
+    }
+    )
+}
